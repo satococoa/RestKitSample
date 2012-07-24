f22f286fbb17fff7009fe9d020a2fc016beec9eb
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-07-24 16:27:24 +0900

    get error message
    2012-07-24 16:27:40.801 RestKitSample[9328:11303] wrong number of
    arguments (-1341117416 for 0) (ArgumentError)

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,27 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    get_user
     true
+  end
+
+  def get_user
+    objectMapping = RKObjectMapping.mappingForClass(GithubUser.class)
+    objectMapping.mapKeyPath('login', toAttribute:'login')
+    objectMapping.mapKeyPath('email', toAttribute:'email')
+    objectMapping.mapKeyPath('name', toAttribute:'name')
+
+    manager = RKObjectManager.objectManagerWithBaseURLString('https://api.github.com')
+    manager.loadObjectsAtResourcePath('/users/satococoa', objectMapping:objectMapping, delegate:self)
+  end
+
+  def objectLoader(objectLoader, didLoadObjects:objects)
+    puts "\e[32m objectLoader:didLoadObjects:\e[0m"
+    user = objects.objectAtIndex(0)
+    NSLog("User Loaded / login: %@, email: %@, name: %@", user.login, user.email, user.name)
+  end
+
+  def objectLoader(objectLoader, didFailWithError:error)
+    puts "\e[31m objectLoader:didFailWithError:\e[0m"
+    NSLog("Encountered an error: %@", error)
   end
 end
---
+++ app/github_user.rb
@@ -0,0 +1,27 @@
+class GithubUser
+  attr_accessor :login, :email, :name
+end
+
+=begin
+{
+  "type": "User",
+  "company": null,
+  "html_url": "https://github.com/satococoa",
+  "following": 25,
+  "location": "Tokyo, Japan",
+  "public_repos": 23,
+  "public_gists": 19,
+  "gravatar_id": "ec3aedf8a29bf5f1dcace93eed9f0015",
+  "login": "satococoa",
+  "email": "e.satoshi@gmail.com",
+  "hireable": false,
+  "avatar_url": "https://secure.gravatar.com/avatar/ec3aedf8a29bf5f1dcace93eed9f0015?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png",
+  "created_at": "2008-10-28T14:39:20Z",
+  "name": "Satoshi Ebisawa",
+  "bio": "",
+  "url": "https://api.github.com/users/satococoa",
+  "followers": 40,
+  "blog": "satococoa.github.com",
+  "id": 31448
+}
+=end
